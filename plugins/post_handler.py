61a5ff76612477f86e57104a485e13a7473746b0
kaustav2576-prog <kaustav2576@gmail.com>
2025-11-13 21:22:53 +0530

    Update post_handler.py

--- plugins/post_handler.py
+++ plugins/post_handler.py
@@ -18,7 +18,7 @@ from utils import temp
 logger = logging.getLogger(__name__)
 post_sessions = {}
 
-BOT_NAME = filestreamkr1bot
+BOT_NAME = temp.U_NAME
 USE_GETFILE_BUTTON_BY_DEFAULT = True
 DEFAULT_WATERMARK = "Join [Filestore4u](https://t.me/Filestore4u)"
 LANGUAGES_FORMAT = "➥ <b>Languages :</b> <code>{langs}</code>"
