2fd25f12b99be49698933e70af00fe13aaaae3cd
kaustav2576-prog <kaustav2576@gmail.com>
2025-11-13 07:46:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/filestore4u') # U
 # ============================
 # User Configuration
 # ============================
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '7307633923').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '7307633923').split()]
 
