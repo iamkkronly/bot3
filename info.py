b6f8185dbb007e4ec01b83cfadd59811b50980a9
kaustav2576-prog <kaustav2576@gmail.com>
2025-11-14 14:03:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/filestorekr')  # Suppor
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002692055617")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1002692055617")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1002692055617 -1002897151507")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
