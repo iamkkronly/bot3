6459e9db5b3a27e81cf0d2d1e89a575ba7e6156c
kaustav2576-prog <kaustav2576@gmail.com>
2025-11-16 13:22:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-1002692055617 -1002897151507"
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', '')    # QR code image for payments
+QR_CODE = environ.get('QR_CODE', 'https://i.ibb.co/jZQPjkMt/Untitled-1.png')    # QR code image for payments
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
